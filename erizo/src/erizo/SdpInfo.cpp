317febfb1bf9d28061d8a09d8374a41b91f82f9b
Paolo Saviano <jenjis2000@hotmail.com>
2013-06-19 11:29:48 +0200

    Fix "a=rtcp-fb:* ccm fir\n";

--- erizo/src/erizo/SdpInfo.cpp
+++ erizo/src/erizo/SdpInfo.cpp
@@ -190,8 +190,7 @@ namespace erizo {
       sdp << "a=ice-pwd:" << icePassword_ << endl;
       sdp << "a=ice-options:google-ice" <<endl;
       sdp << "a=sendrecv" << endl;
-      sdp << "a=mid:video" << endl;
-      sdp << "a=rtcp-fb:* ccm fir\n"
+      sdp << "a=mid:video\n";
       if (isRtcpMux) 
         sdp << "a=rtcp-mux\n";
       for (unsigned int it = 0; it < cryptoVector_.size(); it++) {
@@ -209,7 +208,7 @@ namespace erizo {
           sdp << "a=rtpmap:"<<rtp.payloadType << " " << rtp.encodingName << "/"
             << rtp.clockRate <<"\n";
       }
-
+      sdp << "a=rtcp-fb:* ccm fir\n";
       sdp << "a=ssrc:" << videoSsrc << " cname:o/i14u9pJrxRKAsu" << endl<<
         "a=ssrc:"<< videoSsrc << " msid:"<< msidtemp << " v0"<< endl<<
         "a=ssrc:"<< videoSsrc << " mslabel:"<< msidtemp << endl<<
